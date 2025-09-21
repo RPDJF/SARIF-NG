3a35eb37d6c5589b5fd8cc132dc82097bfc48361
RPDJF <ruipaulo.unify@outlook.fr>
2025-09-21 18:25:19 +0200

    feat: translate pong sidepanel

--- srcs/frontend-v2/src/app/shared/ui/sidebar/panels/pong-panel/pong-panel.component.ts
+++ srcs/frontend-v2/src/app/shared/ui/sidebar/panels/pong-panel/pong-panel.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { I18nState } from '../../../../../state/i18n/i18n.state';
 
 @Component({
   selector: 'app-pong-panel',
@@ -6,4 +8,8 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './pong-panel.component.html',
 })
-export class PongPanelComponent {}
+export class PongPanelComponent {
+  private readonly store = inject(Store);
+
+  public readonly i18n = this.store.selectSignal(I18nState.getI18n);
+}
